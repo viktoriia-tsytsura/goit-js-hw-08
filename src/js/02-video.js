f5970cb168f468ef14ade41924a58064b28eb5cc
viktoriia-tsytsura <sholomviktoriia@gmail.com>
2022-12-20 22:14:55 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    player.on('timeupdate', throttle(onPlay, 1000));
+    
+    function onPlay(data) {
+    console.log(data);
+    const time = data.seconds;
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(time));
+}
+  
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
