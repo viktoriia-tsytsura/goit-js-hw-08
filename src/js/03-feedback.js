c1b6785dc4742b4909ae9b97b7e5e6900ec0bdf0
viktoriia-tsytsura <sholomviktoriia@gmail.com>
2022-12-20 20:47:02 +0100

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = "feedback-form-state";
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+populateInput();
+
+function onInput(evt) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    }));   
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)))
+    
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+   
+}
+
+function populateInput() {
+    const savedInput = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (savedInput) {
+        form.elements.email.value = savedInput.email;
+         form.elements.message.value = savedInput.message;
+    }
+
+}
