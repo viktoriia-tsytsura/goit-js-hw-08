cb9e1ecde4d7f3fbb1a234e1e51173b926e7d3a6
viktoriia-tsytsura <sholomviktoriia@gmail.com>
2022-12-26 12:03:55 +0100

    fix bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,26 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-    const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('play', function() {
+    console.log('played the video!');
+});
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
 
-    player.on('timeupdate', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
     
-    function onPlay(data) {
+function onPlay(data) {
     console.log(data);
     const time = data.seconds;
     localStorage.setItem("videoplayer-current-time", JSON.stringify(time));
 }
-  
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
+
+if (localStorage.length !== 0) {
+  player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
+}
+
